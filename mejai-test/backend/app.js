cdde4923df62e11dc8a8d88661bccc579f1ff6e2
Anthony <nguyenanthony80@gmail.com>
2020-09-29 11:05:33 -0700

    added graphs to frontend

--- mejai-test/backend/app.js
+++ mejai-test/backend/app.js
@@ -4,20 +4,27 @@ const Mejai = require('./Mejai');
 const path = require('path');
 
 const mejai = new Mejai();
+const data = 
+[
+    {
+        matchID: 1238931023890,
+        stacks: [0, 2, 4, 6, 10, 14, 18, 8, 12, 14, 18, 22, 25]
+    },
+    {
+        matchID: 1238931023890,
+        stacks: [0, 2, 4, 6, 0, 4, 0, 2, 6, 0, 4, 0, 2]
+    }
+]
 
-app.set('view engine', 'ejs')
+
 app.set('views', path.join(__dirname, '../frontend2'));
+app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     console.log('main page requested');
-    // mejai.main()
-    // .then(response => {
-    //     res.send(response)
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // })
-    res.render('../frontend2/index');
+
+    res.render('../frontend2/index', {data: data});
 })
 
 
