5ea970f53cf5700566dcd0ddaca252a97b1cbc9e
Anthony <nguyenanthony80@gmail.com>
2020-12-16 12:06:03 -0800

    renamed folders

---
+++ mejai/backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const Mejai = require('./Mejai');
+const path = require('path');
+const {  performance } = require('perf_hooks');
+
+const mejai = new Mejai();
+
+app.set('views', path.join(__dirname, '../frontend-ejs'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    console.log('main page requested');
+    let t0 = performance.now();
+    mejai.main()
+    .then(response => {
+        let t1 = performance.now();
+        console.log("Time: ", t1 - t0);
+        res.render('index', {data: response})
+    })
+    .catch(error => console.log(error.message))
+})
+
+
+app.listen(5000, () => {
+    console.log('Server is running');
+});
